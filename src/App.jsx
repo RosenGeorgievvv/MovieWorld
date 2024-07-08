1d4f385232837528ac4f3483c8bddcb0bbb5f358
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-07-08 11:35:05 +0300

    Update routes in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
-import './App.css'
-import Footer from './components/Footer'
-import Movies from './components/Movies'
-import Navbar from './components/Navbar'
-import Login from './pages/Login'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Movies from "./components/Movies";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Catalog from "./pages/Catalog";
+import About from "./pages/About";
+import Register from "./pages/Register";
 
 function App() {
-
   return (
     <>
-    <div>
-      <Navbar />
-    </div>
-    <Movies />
-    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Movies />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
       <Footer />
-    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
