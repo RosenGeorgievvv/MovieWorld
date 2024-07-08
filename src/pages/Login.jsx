a095004c22cd4b46f5a18d68e8b5a425fde5bc0c
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-07-08 15:31:40 +0300

    Login/Register Authentication
    This update allows user to Register and Login.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import "../styles/Login.css";
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import {auth} from '../services/firebase';
 
 const Login = () => {
+
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+
+const signIn = (e) =>{
+  e.preventDefault();
+  signInWithEmailAndPassword(auth, email, password).then((credentials) =>{
+    console.log(credentials);
+  }).catch((error) =>{
+    console.log(error);
+  })
+}
+
   return (
     <div className='main-form'>
         <div className='main-wrapper'>
             <span className='title'>Login</span>
-            <form>
-                <input type='email' placeholder='email'/>
-                <input type='password' placeholder='password'/>
+            <form onSubmit={signIn}>
+                <input type='email' placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                <input type='password' placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
                 <button>Sign in</button>
             </form>
             <p>You do not have an account? <Link to="/register">Register</Link></p>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Register.css';
+import {auth} from '../services/firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 const Register = () => {
+
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [repass, setRepass] = useState('');
+
+  const signUp = (e) =>{
+    e.preventDefault();
+
+    createUserWithEmailAndPassword(auth, email, password).then((credentials) =>{
+      console.log(credentials);
+    }).catch((error) =>{
+      console.log(error);
+    })
+  }
+
   return (
     <div className='main-form'>
       <div className='main-wrapper'>
         <span className='title'>Register</span>
-        <form>
-          <input type='text' placeholder='Username' />
-          <input type='email' placeholder='Email' />
-          <input type='password' placeholder='Password' />
-          <input type='password' placeholder='Confirm Password' />
+        <form onSubmit={signUp}>
+          <input type='text' placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)}/>
+          <input type='email' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+          <input type='password' placeholder='Confirm Password' value={repass} onChange={(e) => setRepass(e.target.value)}/>
           <button>Register</button>
         </form>
         <p>Already have an account? <Link to="/login">Login</Link></p>
