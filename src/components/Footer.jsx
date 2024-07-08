80676ff0e4678215bde29329443a9cfa838de5f1
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-07-08 11:56:18 +0300

    Footer completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,21 @@
 import React from "react";
+import {FaInstagram, FaFacebook} from 'react-icons/fa';
+import "../styles/Footer.css";
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-left">
-        <p>© All rights reserved.</p>
-        <p>Built by Rosen Georgiev</p>
-      </div>
-      <div className="footer-center">
         <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="social-icon">
           <FaInstagram />
         </a>
         <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="social-icon">
           <FaFacebook />
         </a>
+      </div>
+      <div className="footer-center">
+        <p>© All rights reserved.</p>
+        <p>Built by Rosen Georgiev</p>
       </div>
     </div>
   );
