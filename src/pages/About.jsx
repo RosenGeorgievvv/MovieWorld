585dd468d7142bddd511f70e41ea4d0d64391aa1
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-07-08 12:50:02 +0300

    Make the footer static position

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="about-page">
+      <div className="container">
+        <h1>About MovieLand</h1>
+        <div className='details'>
+        <p>Welcome to MovieLand, your ultimate destination for all things movies!</p>
+        <p>At MovieLand, we are passionate about bringing you the latest news, reviews, trailers, and updates about movies from around the world.</p>
+        <p>Our mission is to provide movie enthusiasts with a comprehensive platform where they can discover, discuss, and engage with their favorite films and genres.</p>
+        <p>Join us in exploring the magic of cinema and stay tuned for exciting updates!</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
