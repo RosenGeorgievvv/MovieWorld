cb5e333c60f9bb04e24f6cf2e1d52e496934cb69
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-07-08 11:13:07 +0300

    Navigation component upgrade

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,20 @@
-import React from "react"; 
+import React from "react";
+import "../styles/Navbar.css";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <p>Home</p>
-      <p>Catalog</p>
-      <p>About</p>
-      <p>Contact</p>
-      <p>Login</p>
-      <p>Register</p> 
-    </div> 
-  );   
+      <ul className="nav-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#catalog">Catalog</a></li>
+        <li><a href="#about">About</a></li>
+      </ul>
+      <ul className="auth-links">
+        <li><a href="#login">Login</a></li>
+        <li><a href="#register">Register</a></li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
