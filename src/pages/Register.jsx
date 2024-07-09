70d16a8338f44e2a623d86df77657d8fc1c4b4b5
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-07-09 17:30:35 +0300

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -28,7 +28,7 @@ const Register = () => {
     <div className='main-form'>
       <div className='main-wrapper'>
         <span className='title'>Register</span>
-        <form onSubmit={signUp} ref={clearInput}>
+        <form onSubmit={signUp} ref={clearInput}> 
           <input type='text' placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)}/>
           <input type='email' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
           <input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
