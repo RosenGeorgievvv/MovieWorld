0b8c7c59723e916cdc09224330c45e133f92a71f
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-07-09 17:31:09 +0300

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -10,7 +10,7 @@ const Register = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [repass, setRepass] = useState('');
- 
+  
   const clearInput = useRef(''); 
 
   const signUp = (e) =>{
