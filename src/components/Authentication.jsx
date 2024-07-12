f284d462d918171f908f0294393a4a5130e64752
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-07-12 12:39:41 +0300

    Update Authentication.jsx
    Within this update, the authentication state is being handled. This mean when new user is registered, his session is kept in state. Then instead of Login and Register again, he will be able see Logout and perform a logout operation.

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import {auth} from '../services/firebase';
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { auth } from '../services/firebase';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
 
-const Authentication = () => {
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const logout = () => {
+    signOut(auth);
+  };
+
   return (
-    <div>Authentication</div>
-  )
-}
-
-export default Authentication
+    <AuthContext.Provider value={{ currentUser, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
