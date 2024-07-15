e40d04e2d186eb36416abc4e5479b54ddaf8f1dc
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-07-15 17:20:11 +0300

    handle edit functionality

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { doc, getDoc } from 'firebase/firestore';
+import { useParams, useNavigate } from 'react-router-dom';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { db } from '../services/firebase';
+import '../styles/Edit.css';
 
 const EditForm = () => {
   const { id } = useParams();
   const [movie, setMovie] = useState(null);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchMovie = async () => {
@@ -14,33 +17,43 @@ const EditForm = () => {
       if (docSnap.exists()) {
         setMovie({ id: docSnap.id, ...docSnap.data() });
       } else {
-        console.log('No such movie!');
+        console.log('No such record!');
       }
     };
 
     fetchMovie();
   }, [id]);
 
+  const handleSave = async (e) => {
+    e.preventDefault();
+    const docRef = doc(db, 'movies', id);
+    await updateDoc(docRef, movie);
+    alert('Movie updated successfully');
+    navigate('/')
+  };
+
   if (!movie) return <div>Loading...</div>;
 
   return (
-    <div>
-      <h1>Edit Movie</h1>
-      <form>
-        <div>
-          <label>Title</label>
-          <input type="text" value={movie.title} onChange={(e) => setMovie({ ...movie, title: e.target.value })} />
-        </div>
-        <div>
-          <label>Image</label>
-          <input type="text" value={movie.image} onChange={(e) => setMovie({ ...movie, Image: e.target.value })} />
-        </div>
-        <div>
-          <label>Description</label>
-          <input type="text" value={movie.description} onChange={(e) => setMovie({ ...movie, Description: e.target.value })} />
-        </div>
-        <button type="submit">Save</button>
-      </form>
+    <div className="main-form">
+      <div className="main-wrapper">
+        <h2 className="title">Edit Movie</h2>
+        <form onSubmit={handleSave}>
+          <div className="form-details">
+            <label>Title</label>
+            <input type="text" value={movie.title} onChange={(e) => setMovie({ ...movie, title: e.target.value })} />
+          </div>
+          <div className="form-details">
+            <label>Image:</label>
+            <input type="file" accept="image/*" onChange={(e) => setMovie({ ...movie, image: e.target.value })} />
+          </div>
+          <div className="form-details">
+            <label>Description:</label>
+            <textarea value={movie.description} onChange={(e) => setMovie({ ...movie, description: e.target.value })} />
+          </div>
+          <button type="submit">Save</button>
+        </form>
+      </div>
     </div>
   );
 };
